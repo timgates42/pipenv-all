45aab543b5b89b8b43c90fc99c7906d4acf1d73d
Tim Gates <tim.gates@iress.com>
2019-06-29 06:58:04 +1000

    Sort out distribution python version first

--- app/install.sh
+++ app/install.sh
@@ -6,20 +6,31 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "${BASEDIR}"
 echo "${PYTHONVERS}"
+
+# Before we go to town with deadsnake get distro pythons in good order
 apt-get update
+apt-get install -qq -y build-essential
+apt-get install -qq -y \
+  python2 python2-pip python2-dev \
+  python3 python3-pip python3-dev
+python2.7 -m pip install --upgrade pip
+python2.7 -m pip install pipenv
+python3.6 -m pip install --upgrade pip
+python3.6 -m pip install pipenv
+
+NODISTRO_PYTHONVERS=$( echo "${PYTHONVERS}" | 's/python2[.]7/ /;s/python3[.]6//' )
+# Now try deadsnakes
 apt-get install -qq -y openssl
 apt-get install -qq -y software-properties-common
 add-apt-repository ppa:deadsnakes
 add-apt-repository ppa:pypy/ppa
 apt-get update
-apt-get install -qq -y ${PYTHONVERS}
+apt-get install -qq -y ${NODISTRO_PYTHONVERS}
 
 apt-get install -qq -y curl
 apt-get install -qq -y libffi-dev
-apt-get install -qq -y build-essential
 
-
-for PYVER in ${PYTHONVERS} ; do
+for PYVER in ${NODISTRO_PYTHONVERS} ; do
   apt-get install -qq -y ${PYVER}-dev
   if [[ "${PYVER}" =~ python[0-9][.][0-9] ]] ; then
     SHORTPY="$(echo "${PYVER}" | sed s/python//)"
@@ -29,7 +40,6 @@ for PYVER in ${PYTHONVERS} ; do
   else
     curl --fail https://bootstrap.pypa.io/get-pip.py -o get-pip.py
   fi
-  SHORTPY=
   "${PYVER}" get-pip.py
   case "${PYVER}" in
   python2.6)
