0df6514d1304a8abb6978d9a1efccf7805e66a1b
Tim Gates <tim.gates@iress.com>
2019-11-22 13:09:14 +1100

    Add 3.8 Remove 3.4

--- ci/shared/_app_helper.sh
+++ ci/shared/_app_helper.sh
@@ -10,9 +10,9 @@ VERSION_PATCH=$(date -u +'%d%H%M%S')
 VERSION="${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}"
 PYTHONVERS="\
     2.7 \
-    3.4 \
     3.5 \
     3.6 \
     3.7 \
+    3.8 \
 "
 export PYTHONVERS
