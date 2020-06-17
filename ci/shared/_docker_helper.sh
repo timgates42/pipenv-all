a0d39cca212dbd5b3e6ae8c04295b328bead76bb
Tim Gates <tim.gates@iress.com>
2020-06-17 11:28:53 +1000

    Update _docker_helper.sh

--- ci/shared/_docker_helper.sh
+++ ci/shared/_docker_helper.sh
@@ -2,7 +2,6 @@
 
 function docker_compose_run() {
     USEROPT="$(id -u):$(id -g)"
-    docker-compose build
     docker-compose up -d
     docker-compose run --rm -u "${USEROPT}" "$@"
     docker-compose down
