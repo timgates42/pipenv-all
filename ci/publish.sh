8f9b8bf0232c91be108d100387684704692d911f
Tim Gates <tim.gates@iress.com>
2020-06-17 10:57:02 +1000

    Rebuild with no cache

--- ci/publish.sh
+++ ci/publish.sh
@@ -8,6 +8,8 @@ BASEDIR="$( dirname "${THISDIR}" )"
 source "${BASEDIR}/ci/shared/_app_helper.sh"
 source "${BASEDIR}/ci/shared/_docker_helper.sh"
 
+docker-compose build --no-cache
+
 docker_compose_run app "/workspace/ci/_test.sh"
 
 docker login
