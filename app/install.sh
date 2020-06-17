270fe967b4904cd70bcdf1c570933dfc9472f1fc
jiashucheniress <Jiashu.Chen@iress.com.au>
2020-06-17 11:43:22 +1000

    removing 2.7 and 3.4 pipfile, use --no-cache-dir when installing python pacakges

--- app/install.sh
+++ app/install.sh
@@ -12,10 +12,9 @@ apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -qq -y --no-install-recommends apt-utils
 DEBIAN_FRONTEND=noninteractive apt-get install -qq -y --no-install-recommends build-essential
 DEBIAN_FRONTEND=noninteractive apt-get install -qq -y --no-install-recommends \
-  python python-pip python-dev \
-  python3 python3-pip python3-dev
-python3.6 -m pip install --upgrade pip
-python3.6 -m pip install pipenv
+  python3 python3-pip python3-dev python3-setuptools
+python3.6 -m pip install --no-cache-dir --upgrade pip
+python3.6 -m pip install --no-cache-dir pipenv
 
 NODISTRO_PYTHONVERS=$( echo "${PYTHONVERS}" | sed 's/2[.]7/ /;s/3[.]6/ /' )
 # Now try deadsnakes
@@ -28,10 +27,11 @@ DEBIAN_FRONTEND=noninteractive apt-get install -qq -y --no-install-recommends cu
 DEBIAN_FRONTEND=noninteractive apt-get install -qq -y --no-install-recommends libffi-dev
 for PYVER in ${NODISTRO_PYTHONVERS} ; do
   DEBIAN_FRONTEND=noninteractive apt-get install -qq -y \
-      --no-install-recommends python${PYVER} python${PYVER}-dev
+      --no-install-recommends "python${PYVER}" "python${PYVER}-dev"
   if ! curl --fail "https://bootstrap.pypa.io/${PYVER}/get-pip.py" -o get-pip.py ; then
     curl --fail https://bootstrap.pypa.io/get-pip.py -o get-pip.py
   fi
   "python${PYVER}" get-pip.py
-  "pip${PYVER}" install pipenv
+  "pip${PYVER}" install --no-cache-dir --upgrade setuptools
+  "pip${PYVER}" install --no-cache-dir pipenv
 done
