6a1340f386a932715874c2a0d43b13a81775888a
Tim Gates <tim.gates@iress.com>
2020-06-17 11:23:12 +1000

    Drop EOL python versions

--- ci/shared/_app_helper.sh
+++ ci/shared/_app_helper.sh
@@ -9,7 +9,6 @@ VERSION_MINOR=$(date -u +'%m')
 VERSION_PATCH=$(date -u +'%d%H%M%S')
 VERSION="${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}"
 PYTHONVERS="\
-    2.7 \
     3.5 \
     3.6 \
     3.7 \
